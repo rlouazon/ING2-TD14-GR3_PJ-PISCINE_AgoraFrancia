f1148ccfb04a6576adeb8328e2a66caf01b5789a
AmauryLBNC <amaury.lubenec@edu.ece.fr>
2024-05-28 16:01:39 +0200

    header - menu

--- inscription.php
+++ inscription.php
@@ -1,4 +1,4 @@
-<?php include("/blocs/header.php") ?>
+<?php include("blocs/header.php") ?>
 
 <head>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
