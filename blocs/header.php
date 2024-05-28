798433e28e29fa67cafcf060babb7f9683edbbf0
AmauryLBNC <amaury.lubenec@edu.ece.fr>
2024-05-28 16:00:35 +0200

    header - menu

--- blocs/header.php
+++ blocs/header.php
@@ -14,3 +14,16 @@
     <title>Agora Francia</title>
 </head>
 <body>
+    <header>
+        <?php $alert = "bonjour"?>
+        agorafrancia
+        <button><a href ="index.php">Accueil</a></button>
+        <button><a href ="parcourir.php">Parcourir</button>
+        <button><a href ="notification.php">Notifications</button>
+        <button><a href ="panier.php">Panier</button>
+        <button><a href ="compte.php">Votre Compte</button>
+        <button><a href ="compte.php">NOM DE COMPTE</button>
+
+    </header>
+
+
