cd5839826640e1573468fd33ba97b57665cc259d
Romain Louazon <romain.louaz@gmail.com>
2024-05-28 11:43:54 +0200

    inscription - ajout du blindage initial

--- blocs/header.php
+++ blocs/header.php
@@ -5,3 +5,12 @@
     $db_found = mysqli_select_db($db_handle, $database);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agora Francia</title>
+</head>
+<body>
