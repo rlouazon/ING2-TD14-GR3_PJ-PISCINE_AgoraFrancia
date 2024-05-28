6ba177d546c1404d62335653d1a636faee152810
AmauryLBNC <amaury.lubenec@edu.ece.fr>
2024-05-28 19:35:35 +0200

    test

--- blocs/footer.php
+++ blocs/footer.php
@@ -2,7 +2,25 @@
     <h2>AGORIA  FRANCIA</h2>
     <p>Mail : contact@agorafrancia.fr | Tel. : +33600000000 | Addr. : 00 rue du petit pont, Paris 75015, France</p>
 </div>
+<script>
+        function checkFooterVisibility() {
+            var footer = document.querySelector('.footer');
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                footer.style.display = 'flex';
+            } else {
+                footer.style.display = 'none';
+            }
+        }
 
+        window.addEventListener('scroll', checkFooterVisibility);
+        window.addEventListener('resize', checkFooterVisibility);
+        document.addEventListener('DOMContentLoaded', function() {
+            checkFooterVisibility();
+            if (document.body.offsetHeight <= window.innerHeight) {
+                document.querySelector('.footer').style.display = 'flex';
+            }
+        });
+    </script>
 
 </body>
 
