e575bac692f76812b514706b9db2cff768723811
Timothee Petit <timfrteam0@gmail.com>
2024-05-29 18:12:07 +0200

    meme CSS pour tout compte

--- mon_compteV.php
+++ mon_compteV.php
@@ -1,5 +1,4 @@
 <?php include("blocs/header.php"); ?>
-<link href="CSS/mon_compteA.css" rel="stylesheet"> <!-- Inclusion du fichier CSS personnalisé -->
 
 <?php
 if($logged != 0){
@@ -33,32 +32,11 @@ if($logged != 0){
         #IMAGES
         $photo =        $data['photo'];
         $back =         $data['back'];
-
-        ?>
-
-
-
-<style>
-body::before{
-    background: center / cover no-repeat url('<?php echo $back; ?>');
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 95%;
-    filter: blur(4px); /* Ajustez le niveau de flou selon vos besoins */
-    z-index: -1; /* Placez l'image de fond floue derrière le contenu */
+    }
 }
+?>
 
-body {
-    font-family: 'Lexend', Arial, sans-serif;
-    margin: 0;
-    padding-top: 90px; /* Ajoutez un padding-top pour éviter que le contenu ne soit caché sous le header fixe */
-    padding-bottom: 60px;
-    position: relative;
-}
-</style>
+<link href="CSS/mon_compteA.css" rel="stylesheet"> <!-- Inclusion du fichier CSS personnalisé -->
 
 <div class="personnal-info"> 
     <div class="titreMain">Informations Personnelles</div>
@@ -67,83 +45,94 @@ body {
 <div class="container">
     <div class="col colG">
         <form method="post">
-            <img src="<?php echo $photo; ?>" alt="photo de profil" class="pp">
+            <img src="photo_user/762cd08c62b6d4.png" alt="photo de profil" class="pp">
             <div class="nomPrenom"> 
-                <h2><?php echo $prenom . " " . $nom; ?></h2>
+                <h2> Stéphane HARDEL</h2>
             </div>
             <div class="info">
                 <label class="naming">Pseudonyme :</label>
-                <label class="info"><?php echo $pseudo; ?></label>
+                <label class="info"> Greg</label>
             </div>
             <div class="info">
                 <label class="naming">Adresse mail :</label>
-                <label class="info"><?php echo $mail; ?></label>
+                <label class="info"> miamlecaca@gmail.com</label>
             </div>
             <div class="info">
                 <label class="naming">Téléphone :</label>
-                <label class="info"><?php echo $tel; ?></label>
+                <label class="info"> 06 65 98 35 92</label>
             </div>
-            <button type="submit" name="Deconnexion" class="btn">Deconnexion    <img src="CSS/images/deconnexion.png" alt="logo" class="imgInscription"></button>
         </form>
-        <?php
-            if(isset($_POST['Deconnexion'])){
-                include('blocs/end_connexion.php');
-            }
-        ?>
     </div>
     <div class="col colD">
         <h2 class="text-center">Informations Bancaires</h2>
         <div class="info">
             <label class="naming">Type de carte :</label>
-            
-            <label class="info"><?php echo (intval($bank_type) < 2) ? ((intval($bank_type) == 0) ? "VISA" : "Mastercard") : ((intval($bank_type) == 2) ? "American Express" : "Paypal"); ?></label>
+            <label class="info"> Visa</label>
         </div>
         <div class="info">
             <label class="naming">Numéro de carte :</label>
-            <label class="info"><?php echo $bank_carte; ?></label>
+            <label class="info"> 0123456789012345 </label>
         </div>
         <div class="info">
             <label class="naming">Nom du titulaire :</label>
-            <label class="info"><?php echo $bank_nom; ?></label>
+            <label class="info"> Greg le mec du meme</label>
         </div>
         <div class="info">
-            <label class="naming">Date d'expiration :</label>
-            <label class="info"><?php echo $bank_date; ?></label>
+            <label class="naming">Date d'éxpiration :</label>
+            <label class="info"> 09/28</label>
         </div>
 
         <h2 class="text-center">Adresse de livraison</h2>
         <div class="info">
             <label class="naming">Adresse 1 :</label>
-            <label class="info"><?php echo $addr1; ?></label>
+            <label class="info"> 32 Chemin du Queric</label>
         </div>
         <div class="info">
             <label class="naming">Adresse 2 :</label>
-            <label class="info"><?php echo $addr2; ?></label>
+            <label class="info"></label>
         </div>
         <div class="info">
             <label class="naming">Ville :</label>
-            <label class="info"><?php echo $ville; ?></label>
+            <label class="info"> La Trinité-sur-Mer</label>
         </div>
         <div class="info">
             <label class="naming">Code Postal :</label>
-            <label class="info"><?php echo $codepostal; ?></label>
+            <label class="info"> 56400</label>
         </div>
         <div class="info">
             <label class="naming">Pays :</label>
-            <label class="info"><?php echo $pays; ?></label>
+            <label class="info"> FRANCE</label>
         </div>
     </div>
 </div>
 
 
-<?php
+    <div class="article-colG col">
+        <div class="prodG">
+            <img src="CSS/images/produit.png" alt="Image produit" class="imgProd">
+            <h2>Nom du produit</h2>
+        </div>
+        <div class="infoProd">
+            <h2>Prix: $XX.XX</h2>
+                <div class="Prod">
+                    Miam le Caca c'est délicieux, je vous la garanti, MIAM MIAM j'ADORE le KK !!
+                    Miam le Caca c'est délicieux, je vous la garanti, MIAM MIAM j'ADORE le KK !!
+                    Miam le Caca c'est délicieux, je vous la garanti, MIAM MIAM j'ADORE le KK !!
+                    Miam le Caca c'est délicieux, je vous la garanti, MIAM MIAM j'ADORE le KK !!
+                    Miam le Caca c'est délicieux, je vous la garanti, MIAM MIAM j'ADORE le KK !!
+                </div>
+            <button class="delete-button">Supprimer l'article</button>
+        </div>
+    </div>
 
-    }
-}
-else{
-    echo "<script>setTimeout(() => window.location.replace(\"connexion.php?redir=".base64_encode($_SERVER['REQUEST_URI'])."\"), 0);</script>";
-}
 
-?>
+
+
+
+
+
+  
+    
+</div>
 
 <?php include("blocs/footer.php"); ?>
