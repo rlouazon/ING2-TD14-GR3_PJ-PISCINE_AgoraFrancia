3394984a1947a908e52163ddd701e2f6ee9fec81
Romain Louazon <romain.louaz@gmail.com>
2024-05-29 17:50:09 +0200

    articles - ajout du chargement des elements

--- articles.php
+++ articles.php
@@ -50,12 +50,17 @@ while ($data = mysqli_fetch_assoc($result)) {
 
 
 
-
-if($occ != 1 || $type == -1 || ((($type == 0) == intval($type_vd)) && (($type == 1) == intval($type_nego)) && (($type == 2) == intval($type_enchere))) == 0){
+# Si :
+#  - Pas dans la bdd ou plusieurs (bug)
+#  - Si pas de type indiqué (empeche de gerer le multi type)
+#  - Si le type ne correspond pas a un de ceux de la bdd (modif manuelle de l'utilisateur ~ triche)
+#  - Si l'annonce est échue
+if($occ != 1   ||   $type == -1   ||   ((($type == 0) == intval($type_vd)) || (($type == 1) == intval($type_nego)) || (($type == 2) == intval($type_enchere))) == 0   ||   $fin == 1){
     echo "<script>setTimeout(() => window.location.replace(\"index.php\"), 0);</script>";
 }
 
 ?>
+
 
 <p><?php echo $vendeur ?></p>
 <p><?php echo $titre ?></p>
@@ -63,12 +68,13 @@ if($occ != 1 || $type == -1 || ((($type == 0) == intval($type_vd)) && (($type ==
 <p><?php echo $description ?></p>
 <p><?php echo $categorie ?></p>
 <p><?php echo $etat ?></p>
-<p><?php echo $type_vd ?></p>
-<h1>TYPE : <?php echo $type; ?></h2>
-<h2>VD : <?php echo (($type == 0) == intval($type_vd)); ?></h2>
-<h2>NEGO : <?php echo (($type == 1) == intval($type_nego)); ?></h2>
-<h2>ENCHERE : <?php echo (($type == 2) == intval($type_enchere)); ?></h2>
-<h2><?php echo ((($type == 0) == intval($type_vd)) && (($type == 1) == intval($type_nego)) && (($type == 2) == intval($type_enchere))) == 1; ?></h2>
+<?php echo ($img1 != "") ? "<img src=\"".$img1."\">" : ""; ?>
+<?php echo ($img2 != "") ? "<img src=\"".$img2."\">" : ""; ?>
+<?php echo ($img3 != "") ? "<img src=\"".$img3."\">" : ""; ?>
+<?php echo ($img4 != "") ? "<img src=\"".$img4."\">" : ""; ?>
+<?php echo ($img5 != "") ? "<img src=\"".$img5."\">" : ""; ?>
+
+
 
 
 <?php include("blocs/footer.php"); ?>
