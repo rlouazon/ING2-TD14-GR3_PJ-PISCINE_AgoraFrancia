d6dacf35f9a8a9101301c188ec48bb21a9ce21d4
Paul Debarbieux <paul.debarbieux@edu.ece.fr>
2024-05-30 23:45:54 +0200

    changement sur la carte google maps

--- index.php
+++ index.php
@@ -8,7 +8,13 @@
     <br>Nam tempor viverra nunc ut tristique. Vestibulum dignissim augue venenatis magna viverra, in aliquet eros pharetra. Curabitur non lectus condimentum nulla dapibus venenatis.
     </p>
     <p>
-
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et nibh sed eros consequat imperdiet. Cras pretium vestibulum nibh, tristique accumsan tortor malesuada vel. Nulla facilisi. Praesent ac enim nec justo dictum facilisis tincidunt nec augue. 
+    <br>Donec sit amet sapien volutpat, ornare nisi id, dictum diam. Nam imperdiet nisl in varius aliquet. Sed aliquam metus quis ex ultricies, id commodo lorem luctus. Nulla rhoncus tempor dolor, eu sodales urna accumsan quis. In vulputate in velit eleifend rhoncus. 
+    <br>Sed posuere augue sit amet leo scelerisque dictum. Donec eu eros ac ligula lacinia fermentum. Duis ut justo et lacus ultricies eleifend a et est.
+    <br>Vivamus luctus viverra tortor, id iaculis ligula condimentum vitae. Curabitur sollicitudin blandit accumsan. Maecenas tristique mauris magna, nec accumsan nibh condimentum at. Quisque tortor arcu, hendrerit et orci vitae, interdum pulvinar velit. Nulla facilisi. 
+    <br>Suspendisse finibus eleifend tincidunt. Ut id urna ut elit fringilla ornare tincidunt eget nibh. Aenean sit amet purus ac massa faucibus porttitor vel sollicitudin purus. Sed consequat neque augue. Nullam et semper sapien. Sed quis imperdiet nunc. 
+    <br> ultricies massa neque, nec porta mi rhoncus nec. Nunc vestibulum felis eu lacus euismod, fermentum posuere metus suscipit. Nullam nisl ante, condimentum eget accumsan quis, mattis in orci.
+    Sed cursus, purus eu aliquet venenatis, leo eros lacinia magna, gravida consectetur felis velit eget nulla. Aliquam erat volutpat. Nunc risus lectus, faucibus quis sem eget, ornare condimentum metus. Maecenas ut eros nisi. Donec eu dolor sodales, blandit libero sit amet, rutrum velit. In porta nunc et dignissim fermentum. Duis ligula enim, ultricies et leo id, luctus dictum leo. Aenean laoreet egestas accumsan. Duis quis eros ligula. Sed pellentesque est blandit diam pulvinar ullamcorper. Aliquam eget sapien eget felis rhoncus fermentum. Proin at ante at dui tristique elementum vitae et quam. Pellentesque non neque non massa lobortis tincidunt
     </p>
     <div class="personnal-info" id = "une"> 
         <div class="titreMain">A la une !</div>
