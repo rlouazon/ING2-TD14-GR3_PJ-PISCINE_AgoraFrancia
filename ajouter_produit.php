76461b3876356b9a4c9cfc92b9d216103adb20fa
Romain Louazon <romain.louaz@gmail.com>
2024-05-31 17:13:55 +0200

    articles - encheres

--- ajouter_produit.php
+++ ajouter_produit.php
@@ -1,6 +1,48 @@
 <?php include("blocs/header.php"); ?>
 <link href="CSS/inscription.css" rel="stylesheet"> <!-- Inclusion du fichier CSS personnalisé -->
 
+<?php
+$type_compte = 0;
+if($logged != 0){
+    $requete = "SELECT * FROM utilisateurs WHERE id = " . $logged;
+    $result = mysqli_query($db_handle, $requete);
+    while ($data = mysqli_fetch_assoc($result)) {
+        #ID
+        $id =           $data['id'];
+        $type =         $data['type'];
+        $type_compte = $type;
+
+        #INFO PERSO
+        $pseudo =       $data['pseudo'];
+        $nom =          $data['nom'];
+        $prenom =       $data['prenom'];
+        $mail =         $data['mail'];
+        $tel =          $data['tel'];
+
+        #BANQUE
+        $bank_type =    $data['bank_type'];
+        $bank_carte =   $data['bank_carte'];
+        $bank_nom =     $data['bank_nom'];
+        $bank_date =    $data['bank_date'];
+
+        #ADDRESSE
+        $addr1 =        $data['addr1'];
+        $addr2 =        $data['addr2'];
+        $ville =        $data['ville'];
+        $codepostal =   $data['codepostal'];
+        $pays =         $data['pays'];
+
+        #IMAGES
+        $photo =        $data['photo'];
+        $back =         $data['back'];
+    }
+}
+
+$success = 0;
+
+if($logged != 0 && $type_compte != 0){
+?>
+
 <?php if($logged==0){
     echo "<script>setTimeout(() => window.location.replace(\"index.php\"), 0);</script>";
 }
@@ -149,10 +191,18 @@ if(isset($_POST['ajout_du_produit'])){
 
     if ($result) {
         $alert = "Produit ajouté avec succès";
+        $success = 1;
     } else {
         $alert = "Erreur lors de l'ajout du produit : " . mysqli_error($db_handle);
     }
 }
+
+}
+
+
+include("blocs/redir.php");
+
+
 ?>
 
 <?php include("blocs/footer.php"); ?>
