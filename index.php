f4ab91197ab1796e3fdd2f0fb7a32049e6081062
Timothee Petit <timfrteam0@gmail.com>
2024-05-31 13:32:45 +0200

    fix bug

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php include("blocs/header.php"); ?>
 <link href="CSS/index.css" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="CSS/index.css" rel="stylesheet">
+    <title>Agora Francia</title>
+</head>
 <body>
-    <?php include("blocs/header.php"); ?>
 
-    <div class="personnal-info" id="une">
+    <div class="personnal-info" id = "une"> 
         <div class="titreMain">A la une !</div>
     </div>
     
