4c75d00f4243d7daa390527019ded32ffa7efa62
Timothee Petit <timfrteam0@gmail.com>
2024-06-01 11:25:49 +0200

    d

--- panier.php
+++ panier.php
@@ -38,95 +38,97 @@ if($logged != 0){
 
 <link href="CSS/mon_compteA.css" rel="stylesheet"> <!-- Inclusion du fichier CSS personnalisé -->
 
-<div class="personnal-info" id="panier"> 
-    <div class="titreMain">Mon Panier</div>
-</div>
-
-<div class="container">
-    <div class="col colG">
-        <form method="post">
-        <h2 class="text-center">Résumé de commande</h2>
-        <div class="info">
-            <label class="info"> Sous total :</label>
-        </div>
-
-        <div class="info">
-        <label class="naming">- Gode or platine ancéstral</label>
-        <label class="info"> 2 198€</label>  
-    </div>
-    <div class="info">
-        <label class="naming">- Saucisse taille M parcque c'est trop bon</label>
-        <label class="info"> 2.98€</label>  
+<div class="flou">
+    <div class="personnal-info" id="panier"> 
+        <div class="titreMain">Mon Panier</div>
     </div>
 
-    <div class="info">
-        <label class="info"> Promotion :</label>
-    </div>
-    <div class="info">
-        <label class="naming">Promotion :</label>
-        <label class="info"> 0.00€</label>  
-    </div>
+    <div class="container">
+        <div class="col colG">
+            <form method="post">
+            <h2 class="text-center">Résumé de commande</h2>
+            <div class="info">
+                <label class="info"> Sous total :</label>
+            </div>
 
-    <div class="info">
-        <label class="info"> Livraisons :</label>
-    </div>
-    <div class="info">
-        <label class="naming">Frais de livraisons :</label>
-        <label class="info"> 0.00€</label>  
-    </div>
-
-    <h2 class="text-center">Total</h2>
-    <div class="info">
-        <label class="naming">Total de la commande :</label>
-        <label class="info"> 2200.98€</label>  
-    </div>
-
-    <button class="validation-button">Passer à la caisse</button>
-
-    </div>
-
-
-
-    <div class="col colD">
-
-        <h2 class="text-center">Informations Bancaires</h2>
-        <div class="info">
-            <label class="naming">Type de carte :</label>
-            <label class="info"> Visa</label>
+            <div class="info">
+            <label class="naming">- Gode or platine ancéstral</label>
+            <label class="info"> 2 198€</label>  
         </div>
         <div class="info">
-            <label class="naming">Numéro de carte :</label>
-            <label class="info"> 0123456789012345 </label>
-        </div>
-        <div class="info">
-            <label class="naming">Nom du titulaire :</label>
-            <label class="info"> Greg le mec du meme</label>
-        </div>
-        <div class="info">
-            <label class="naming">Date d'éxpiration :</label>
-            <label class="info"> 09/28</label>
+            <label class="naming">- Saucisse taille M parcque c'est trop bon</label>
+            <label class="info"> 2.98€</label>  
         </div>
 
-        <h2 class="text-center">Adresse de livraison</h2>
         <div class="info">
-            <label class="naming">Adresse 1 :</label>
-            <label class="info"> 32 Chemin du Queric</label>
+            <label class="info"> Promotion :</label>
         </div>
         <div class="info">
-            <label class="naming">Adresse 2 :</label>
-            <label class="info"></label>
+            <label class="naming">Promotion :</label>
+            <label class="info"> 0.00€</label>  
+        </div>
+
+        <div class="info">
+            <label class="info"> Livraisons :</label>
         </div>
         <div class="info">
-            <label class="naming">Ville :</label>
-            <label class="info"> La Trinité-sur-Mer</label>
+            <label class="naming">Frais de livraisons :</label>
+            <label class="info"> 0.00€</label>  
         </div>
+
+        <h2 class="text-center">Total</h2>
         <div class="info">
-            <label class="naming">Code Postal :</label>
-            <label class="info"> 56400</label>
+            <label class="naming">Total de la commande :</label>
+            <label class="info"> 2200.98€</label>  
         </div>
-        <div class="info">
-            <label class="naming">Pays :</label>
-            <label class="info"> FRANCE</label>
+
+        <button class="validation-button">Passer à la caisse</button>
+
+        </div>
+
+
+
+        <div class="col colD">
+
+            <h2 class="text-center">Informations Bancaires</h2>
+            <div class="info">
+                <label class="naming">Type de carte :</label>
+                <label class="info"> Visa</label>
+            </div>
+            <div class="info">
+                <label class="naming">Numéro de carte :</label>
+                <label class="info"> 0123456789012345 </label>
+            </div>
+            <div class="info">
+                <label class="naming">Nom du titulaire :</label>
+                <label class="info"> Greg le mec du meme</label>
+            </div>
+            <div class="info">
+                <label class="naming">Date d'éxpiration :</label>
+                <label class="info"> 09/28</label>
+            </div>
+
+            <h2 class="text-center">Adresse de livraison</h2>
+            <div class="info">
+                <label class="naming">Adresse 1 :</label>
+                <label class="info"> 32 Chemin du Queric</label>
+            </div>
+            <div class="info">
+                <label class="naming">Adresse 2 :</label>
+                <label class="info"></label>
+            </div>
+            <div class="info">
+                <label class="naming">Ville :</label>
+                <label class="info"> La Trinité-sur-Mer</label>
+            </div>
+            <div class="info">
+                <label class="naming">Code Postal :</label>
+                <label class="info"> 56400</label>
+            </div>
+            <div class="info">
+                <label class="naming">Pays :</label>
+                <label class="info"> FRANCE</label>
+            </div>
         </div>
     </div>
 </div>
